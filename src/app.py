f54509bf56f4e1e1fa33739255d8978af61c1a92
Alberto Garcia <algarna85@gmail.com>
2022-09-06 16:50:07 +0200

    Fixed bug and added comments

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from datetime import *
 from dateutil.relativedelta import *
 
+#Print actual date
 now = datetime.now()
 print(now)
 
-now = now + relativedelta(months=1, weeks=1, hour=5)
+#Print the new date after adding months, weeks and hours
+now = now + relativedelta(months=1, weeks=1, hours=5)
 print(now)
